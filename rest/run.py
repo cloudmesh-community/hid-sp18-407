a6bdb2f859413cc8a1df0e3d59b1c7dba462bdff
Keith Hickman <34774103+keithhickman08@users.noreply.github.com>
2018-02-04 19:37:34 -0500

    Update run.py

--- rest/run.py
+++ rest/run.py
@@ -1,6 +1,32 @@
 from eve import Eve
+from student import Student
+import platform
+import psutil
+import json
+from flask import Response
 
 app = Eve()
 
+@app.route('/times', methods=['GET'])
+
+def times():
+	times = {}
+	alltimes = psutil.cpu_times('/')
+	times['user'] = alltimes.user
+	times['system'] = alltimes.system 
+	times['idle'] = alltimes.idle
+
+	return times
+
+def memory():
+	memory = {}
+	allmem = psutil.virtual_memory('/')
+	memory['totmem'] = allmem.total
+	memory['availmem'] = allmem.available
+	memory['usedmem'] = allmem.used
+	memory['freemem'] = allmem.free
+	return memory
+
+
 if __name__ == '__main__':
-    app.run()
+	app.run()
