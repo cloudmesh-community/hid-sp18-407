04bf199187bf7347a43b6ee7708f59962ef654da
Keith Hickman <34774103+keithhickman08@users.noreply.github.com>
2018-02-04 19:38:05 -0500

    Update settings.py

--- rest/settings.py
+++ rest/settings.py
@@ -1,17 +1,5 @@
 MONGO_HOST = 'localhost'
 MONGO_PORT = '27017'
-MONGO_DBNAME = 'student_db'
-DOMAIN = {
-    'student':{
-        'schema':{
-            'firstname': {'type': 'string'},
-            'lastname': {'type': 'string'},
-            'school': {'type': 'string'},
-            'university': {'type':'string'},
-            'email':{'type':'string', 'unique':True}
-        }
-
-    }
-}
-
-RESOURCE_METHODS =['GET', 'POST']
+MONGO_DBNAME = 'system_info'
+DOMAIN = {}
+RESOURCE_METHODS = ['GET']
