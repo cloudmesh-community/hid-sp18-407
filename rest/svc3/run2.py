3fdaf2f59032a27e7f5d34f52ce3466a6d4999cd
Keith Hickman <34774103+keithhickman08@users.noreply.github.com>
2018-02-17 08:27:24 -0500

    Update run2.py

--- rest/svc3/run2.py
+++ rest/svc3/run2.py
@@ -6,18 +6,28 @@ import json
 from flask import Response
 
 app = Eve()
+@app.route('/times', methods=['GET'])
+def times():
+	timeInfo = str(psutil.cpu_times())
+	#cdata = json.dumps(timeInfo.__dict)
+	#response = Response()
+	#response.headers["status"] = 200
+	#response.headers["Content-Type"] = "application/json; charset=utf-8"
+	#response.data = cdata
+	return timeInfo
+  
 @app.route('/computer', methods=['GET','POST'])
-
 def computerInfo():
-	
-	compInfo = Computer("8GB", "201GB", "1s")
+	# "Here's my computer info"
+	compInfo = Computer("24GB", "201GB", "1s")
 	cdata = json.dumps(compInfo.__dict__)
 	response = Response()
 	response.headers["status"] = 200
 	response.headers["Content-Type"] = "application/json; charset=utf-8"
 	response.data=cdata
 	return response
-	# return "My name is John"
 
 if __name__ == '__main__':
 	app.run()
+
+# computerInfo()
