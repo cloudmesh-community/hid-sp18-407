00a0341eb25b22a60962ffdfd1c760e7f2ba1080
keithhickman08 <keonhick@iu.edu>
2019-07-25 15:44:26 -0400

    added julia-app.jl

--- julia-aws/julia-app.jl
+++ julia-aws/julia-app.jl
@@ -1,13 +1,23 @@
-import Pkg
 Pkg.add("AWSCore")
+Pkg.add("Retry")
+Pkg.add("SymDict")
+#Pkg.add("AWSIAM")
+Pkg.add("AWSSDK")
+Pkg.add("AWSS3")
 
+#using AWSEC2
 using AWSS3
-
 using AWSCore.Services
 
-Services.cloudformation("CreateStack",
-               StackName = "AWSJuliaStack",
-               TemplateBody = read("template-test.yaml"),
-               #Parameters = [["InstanceType"   => "t2.micro"]],
-               Capabilities = ["CAPABILITY_IAM"])
+aws = AWSCore.Services.aws_config()
+s3_create_bucket(aws, "myjuliabucket")
+s3_enable_versioning(aws, "myjuliabucket")
+s3_put(aws,"myjuliabucket", "key", "Hello!")
 
+# cloudformation("CreateStack",
+#               StackName = "AWSJuliaStack",
+#               TemplateBody = read("template.yaml"),
+#               #Parameters = [["InstanceType"   => "t2.micro"]],
+#               Capabilities = ["CAPABILITY_IAM"])
+
+#AWSS3.s3_list_buckets()
